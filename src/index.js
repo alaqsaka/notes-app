2696cdd88f8208cc87e7ef6f8e4e71a50a2d6b27
alaqsaka <aqsaabidin15@gmail.com>
2022-12-25 15:06:25 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import NoteApp from "./components/NoteApp";
-import { BrowserRouter } from "react-router-dom";
-// import style
-import "./styles/style.css";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-const root = createRoot(document.getElementById("root"));
+import './styles/style.css';
+
+const root = createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <NoteApp />
-  </BrowserRouter>
+  <App />,
 );
